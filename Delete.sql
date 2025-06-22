a34d24a65396cbedcdacdd1d28633787650f73d8
Anton-Ferreira <antonioferr.alcantara@gmail.com>
2025-06-22 20:02:55 -0300

    Add files via upload

---
+++ Delete.sql
@@ -0,0 +1,21 @@
+DELETE FROM cliente WHERE id = 1;
+
+DELETE FROM loja WHERE id = 1;
+
+DELETE FROM categoria_veiculo WHERE id = 1;
+
+DELETE FROM veiculo WHERE id = 1;
+
+DELETE FROM manutencao WHERE id = 1;
+
+DELETE FROM motorista WHERE id = 1;
+
+DELETE FROM periodo_locacao WHERE id = 1;
+
+DELETE FROM reserva WHERE id = 1;
+
+DELETE FROM locacao WHERE id = 1;
+
+DELETE FROM pagamento WHERE id = 1;
+
+DELETE FROM funcionario WHERE id = 1;
