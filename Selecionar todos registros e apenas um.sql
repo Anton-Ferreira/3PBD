3192a8f13788ff7335dd3a0ac02740cbb3867a39
Anton-Ferreira <antonioferr.alcantara@gmail.com>
2025-06-22 20:26:54 -0300

    Add files via upload

---
+++ Selecionar todos registros e apenas um.sql
@@ -0,0 +1,17 @@
+--Para selecionar todos registros(linhas)
+
+SELECT * FROM cliente;
+
+-- Todas as lojas
+SELECT * FROM loja;
+
+-- Todas as categorias de veículos
+SELECT * FROM veiculo;
+
+--Para selecionar apenas um registro(linha)
+
+SELECT * FROM cliente WHERE id = 1;
+
+SELECT * FROM loja WHERE id = 1;
+
+SELECT * FROM veiculo WHERE id = 1;
