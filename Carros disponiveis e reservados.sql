96dfbcc481d79e845dd4de42a8d2539132d3cc59
Anton-Ferreira <antonioferr.alcantara@gmail.com>
2025-06-29 18:31:35 -0300

    Add files via upload

---
+++ Carros disponiveis e reservados.sql
@@ -0,0 +1,22 @@
+FROM veiculo v
+WHERE v.id NOT IN (
+    SELECT r.id_veiculo
+    FROM reserva r
+    WHERE DATE('2025-07-10') BETWEEN DATE(r.Data_previstaRetirada) AND DATE(r.Data_previstaDevolucao)
+)
+
+AND v.id NOT IN (
+    SELECT m.id_veiculo
+    FROM manutencao m
+    WHERE '2025-07-10' BETWEEN DATE(m.data_inicio) AND DATE(m.data_fim)
+);
+
+SELECT 
+    v.id,
+    v.modelo,
+    v.placa,
+    r.data_previstaRetirada,
+    r.data_previstaDevolucao
+FROM veiculo v
+INNER JOIN reserva r ON v.id = r.id_veiculo
+WHERE '2025-07-10' BETWEEN DATE(r.data_previstaRetirada) AND DATE(r.data_previstaDevolucao);
